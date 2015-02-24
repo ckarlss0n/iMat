a720babeda07e63c1ce82a5d0bc8ede38fcda67b
emiilax <emilaxelsson@Emils-MacBook-Pro.local>
2015-02-24 22:45:36 +0100

    Added SmallFoodPanels fxml + class

--- iMatApplication/src/application/MainPanel.java
+++ iMatApplication/src/application/MainPanel.java
@@ -1,10 +1,17 @@
 package application;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
+import javax.swing.ImageIcon;
+
+import se.chalmers.ait.dat215.project.IMatDataHandler;
+import se.chalmers.ait.dat215.project.Product;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 
@@ -32,7 +39,11 @@ public class MainPanel extends AnchorPane {
 	int i = 0;
 	public void buttonClicked(ActionEvent evt){
 		
+		Product prod = new Product();
+		IMatDataHandler im = IMatDataHandler.getInstance();
+		List<Product> produ = im.getProducts();
 		
+		System.out.println(produ.size());
 		if(i == 0){
 			
 			ProfilePanel pp = new ProfilePanel();
@@ -41,7 +52,19 @@ public class MainPanel extends AnchorPane {
 		} else{
 			stackPane.getChildren().clear();
 			OfflinePanel op = new OfflinePanel();
-			stackPane.getChildren().addAll(op);
+			OnlinePanel onp = new OnlinePanel();
+			
+			for(int i = 0; i < 4; i++){
+				SmallProductPanel smpp = new SmallProductPanel();
+				Product p = produ.get(i);
+				File fimg = new File(im.getImageIcon(p).getDescription());
+				System.out.println(fimg.toURI().toString());
+				Image image = new Image(fimg.toURI().toString());
+				smpp.setProductImage(image);
+				smpp.setProductName(p.getName());
+				onp.add(smpp, i);
+			}
+			stackPane.getChildren().addAll(onp);
 		}
 		
 		System.out.println("Click");

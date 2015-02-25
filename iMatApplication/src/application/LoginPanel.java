857f263fc169912b24602ef8eac94ea5927683f1
vchristoffersson <vchristoffersson@gmail.com>
2015-02-25 15:07:55 +0100

    login
    loginPanel now reads email and password to check if there's a match in
    user.txt gives support to multiple users

--- iMatApplication/src/application/LoginPanel.java
+++ iMatApplication/src/application/LoginPanel.java
@@ -1,9 +1,11 @@
 package application;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
@@ -46,10 +48,44 @@ public class LoginPanel extends Pane{
 	private TextField txtfPassword;
 	
 	BufferedWriter writer = null;
+	BufferedReader reader;
 	public boolean logIn(ActionEvent evt){
 		String username = txtfUsername.getText();
 		String password = txtfPassword.getText();
+		String usernameRead = null;
+		String passwordRead = null;
+		String line;
 		
+		try {
+			
+			reader = new BufferedReader(new FileReader("user.txt"));
+			
+			while((line = reader.readLine()) != null){
+        		if(line.equals("#email")){
+        			usernameRead = reader.readLine();
+        		}
+        		if(line.equals("#password")){
+        			passwordRead = reader.readLine();
+       			}    			
+        		if (usernameRead.equals(username) && passwordRead.equals(password)){
+        			reader.close();
+        			return true;
+        		}	
+        	}
+			reader.close();
+		} catch (NullPointerException e) {
+			e.printStackTrace();
+			System.out.println("no matching user found");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch	(IOException e) {
+			e.printStackTrace();
+		} finally {
+			return false;
+		}
+		
+		
+		/*
 		User user = IMatDataHandler.getInstance().getUser();
 		if(username.equals(user.getUserName()) && password.equals(user.getPassword())){
 			System.out.println("True");
@@ -57,7 +93,7 @@ public class LoginPanel extends Pane{
 		}else{
 			System.out.println("False");
 			return false;
-		}
+		}*/
 	}
 	
 

8b9b8a3f7566b75c175417e8df9fbdb2bf6ce647
vchristoffersson <vchristoffersson@gmail.com>
2015-03-03 15:45:27 +0100

    progressindicator

--- iMatApplication/src/application/ProcessIndicator.java
+++ iMatApplication/src/application/ProcessIndicator.java
@@ -2,16 +2,26 @@ package application;
 
 import java.io.IOException;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ProgressIndicator;
 import javafx.scene.layout.BorderPane;
 
 public class ProcessIndicator extends BorderPane {
+	
+	@FXML 
+	ProgressIndicator progressOverview;
+	@FXML 
+	ProgressIndicator progressPersInfo;
+	@FXML 
+	ProgressIndicator progressChoosePayment;
+	@FXML 
+	ProgressIndicator progressFinished;
 	
 	public ProcessIndicator(){
 		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("progressIndicator.fxml"));
         fxmlLoader.setRoot(this);
         fxmlLoader.setController(this);
-
         try {
             fxmlLoader.load();
         } catch (IOException exception) {

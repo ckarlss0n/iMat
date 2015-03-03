5d836a5d54f0ca8c5db458f1754e8b4fff21b30c
vchristoffersson <vchristoffersson@gmail.com>
2015-03-03 17:04:09 +0100

    no

--- iMatApplication/src/application/ShoppingCartBig.java
+++ iMatApplication/src/application/ShoppingCartBig.java
@@ -13,6 +13,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBoxBuilder;
@@ -44,6 +45,8 @@ public class ShoppingCartBig extends BorderPane {
 	private Button btnLoginCart;
 	@FXML
 	private Button shopWithoutBtn;
+	@FXML
+	private Label bigCartSum;
 	
 	public void shopWithout(ActionEvent evt){
 		
